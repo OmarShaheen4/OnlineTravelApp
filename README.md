4d3141c443753da5b38523c327410c9b52578af5
OmarShaheen4 <110252828+OmarShaheen4@users.noreply.github.com>
2022-08-14 12:00:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Project Title
+Weather-Journal App Project
+
+This project is a part of [Udacity](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011) Front End Developer Nanodegree program.
+
+## Table of Contents
+- [Project Title](#project-title)
+- [Table of contents](#table-of-contents)
+- [Languages Used](#languages-used)
+- [Description](#description)
+- [Motivation](#motivation)
+- [Installation](#installation)
+- [Contribute](#contribute)
+- [ScreenShots](#screenshots)
+- [License & Copyright](#license--copyright)
+
+## Languages used
+1. HTML
+2. CSS
+3. JAVASCRIPT
+4. Node 
+5. Express  
+
+[(Back to top)](#table-of-contents)
+
+## Description
+Apply your new skills to combine data from the OpenWeatherMap API and client side (browser) HTML forms to create a web app that records a weather journal for users. This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI for a Weather Journal App.
+
+[(Back to top)](#table-of-contents)
+
+## Motivation
+This project is made to show how can you contact api and get data from it.
+
+[(Back to top)](#table-of-contents)
+
+## Installation
+- You can Install this project to your PC using clone the repo to your github account then Download It as a ZIP File to your PC.
+- Install express, cors, body-parser (npm install express ...)
+- In the terminal or cmd write node server.js to run local server
+- go to website and wirte http://localhost:3300/ 
+
+[(Back to top)](#table-of-contents)
+
+## Contribute
+-Udacity
+
+[(Back to top)](#table-of-contents)
+
+## Screenshots
+<img src="screenShoot1.png">
+<img src="screenShoot2.png">
+
+[(Back to top)](#table-of-contents)
+
+## License & Copyright
+Udacity © Omar Shaheen
+
+[(Back to top)](#table-of-contents)
+
+## Overview
+This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+[(Back to top)](#table-of-contents)
+
+## Instructions
+This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
+
+[(Back to top)](#table-of-contents)
+
+## License & Copyright
+Udacity © Omar Shaheen
+
+[(Back to top)](#table-of-contents)
---
+++ website/app.js
@@ -0,0 +1,116 @@
+/* Global Variables */
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather/';
+
+// Instead of Converting Temperature from Kelvin to Celsius or Fahrenheit use: 
+// &units=imperial is for Fahrenheit (or)
+// &units=metric is for Celcius.
+const apiKey = 'bb04270cd184efee40dac170417a61a3&units=metric';
+
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+const generateBtn = document.getElementById('generate');
+generateBtn.addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction(e) {
+    // This is going to stop any action occurred in the button 
+    e.preventDefault();
+
+    //get user input
+    const zipCode = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+
+    // Make sure that the user enter a zipcode 
+    if (zipCode !== '') {
+        // Make sure that the user enter how he feels
+        if (content !== '') {
+            /* Function to GET Web API Data*/
+            getWeatherData(baseUrl, zipCode, apiKey)
+                .then(function (data) {
+                    console.log('The data');
+                    console.log(data);
+                    // add data to POST request
+                    postData('/AddData', { temp: data.main.temp, date: newDate, content: content });
+                }).then(function () {
+                    // call updateUI to update browser content
+                    updateUI()
+                }).catch(function (error) {
+                    console.log(error);
+                    alert('The zip code is invalid. Try again');
+
+                });
+        } else {
+            alert('Please Enter How do you feel')
+        }
+    } else {
+        alert('Please Enter ZipCode')
+    }
+
+}
+
+
+/* Function to GET Web API Data by generating url and path what the user 
+    enter to get the data from the API*/
+const getWeatherData = async (baseUrl, zipCode, apiKey) => {
+    // res equals to the output of fetch function
+    const res = await fetch(`${baseUrl}?zip=${zipCode}&apikey=${apiKey}`);
+    // const res = await fetch(baseUrl +zipCode+`/&appid=${apiKey}`);
+    console.log(res)
+    try {
+        // data equals to res but in json format
+        const data = await res.json();
+
+        console.log('The data from weatherData');
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+
+    });
+
+    console.log('response');
+    console.log(response);
+    try {
+        const newData = await response.json();
+        console.log('newData');
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        console.log("all data")
+        console.log(allData);
+        // update new entry values
+        if (allData.date !== undefined && allData.temp !== undefined && allData.content !== undefined) {
+            // Write updated data to DOM elements
+
+            document.getElementById('date').innerHTML = `Today's Date: ` + allData.date;
+            document.getElementById('temp').innerHTML = `Today's Temperature: ` + Math.round(allData.temp) + ' degrees Celsius';
+            document.getElementById('content').innerHTML = `You are Feeling: ` + allData.content;
+        }
+    } catch (error) {
+        console.log('error', error);
+    }
+};
